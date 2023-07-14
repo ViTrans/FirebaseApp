35f69174f4d12b9f44a597064aaf17a93d065e8a
ViTrans <93078368+ViTrans@users.noreply.github.com>
2023-07-14 21:05:45 +0700

    fix api and ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import lodash from "lodash";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import WeatherItem from "../components/weather/WeatherItem";
 const HomePage = () => {
   const [city, setCity] = useState("hanoi");
   const [weather, setWeather] = useState(null);
   //   const [error, setError] = useState(null);
   //   const [loading, setLoading] = useState(false);
 
+  const api = {
+    key: "57f175522f1771d7bc35c2ab31348f29",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
   const getWeather = async (city) => {
     try {
       const response = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=a4c5421eb28949f08f9151545232906&q=${
-          city || "hanoi"
-        }&aqi=yes`
+        `${api.base}forecast?q=${city || "hanoi"}&units=metric&APPID=${api.key}`
       );
       setWeather(response.data);
     } catch (error) {
@@ -21,62 +27,80 @@ const HomePage = () => {
   };
   useEffect(() => {
     getWeather(city);
+    console.log(weather);
   }, [city]);
 
   const handleChangeCity = lodash.debounce((e) => {
     setCity(e.target.value);
   }, 1000);
 
-  // https://api.weatherapi.com/v1/current.json?key=2643b73877f14c8183e144152232706&q=hanoi&aqi=yes
+  const { user } = useAuth();
+
+  const navigate = useNavigate();
+  if (!user) {
+    return navigate("/signin");
+  }
+
   return (
-    <div className="max-w-[600px] bg-white shadow-md rounded-lg mx-auto p-3 mt-20">
-      <div className="flex items-center w-full gap-5 px-5 py-3 border border-gray-200 rounded-lg">
-        <input
-          onChange={handleChangeCity}
-          type="text"
-          className="w-full bg-transparent outline-none"
-          placeholder="Enter your content..."
-        />
-        <span className="flex-shrink-0 text-gray-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </span>
-      </div>
-      <h3 className="mt-2 text-xl font-medium text-center">Select City</h3>
-      <div>
-        {weather && (
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <h4 className="text-xl font-medium">{weather?.location?.name}</h4>
-              <div className="flex items-center gap-x-5">
-                <p className="text-gray-500 ">
-                  {weather?.current?.condition?.text}
-                </p>
-                <img
-                  className="w-[30px] h-[30px]"
-                  src={weather?.current?.condition?.icon}
-                  alt=""
-                />
-              </div>
-            </div>
-            <span className="text-xl font-medium">
-              {weather?.current?.temp_c}
-              <sup>o</sup>C
+    <div className="w-full bg-white shadow-md rounded-lg mx-auto relative ">
+      <img src="bg.jpg" alt="" />
+      <div className="absolute top-0 left-0 w-full h-full ">
+        <div className="flex justify-between">
+          <div className="flex flex-col p-5 m-5 text-white bg-white bg-opacity-25 w-full max-w-[300px] rounded-md">
+            <h1 className="text-4xl font-medium text-white ">
+              {new Date(weather?.list[0].dt_txt).toLocaleDateString("en-US", {
+                weekday: "long",
+              })}
+            </h1>
+            <span>
+              {new Date(weather?.list[0].dt_txt).toLocaleDateString("en-US", {
+                day: "numeric",
+                month: "long",
+              })}
             </span>
+            <span className="text-6xl font-bold">
+              {Math.round(weather?.list[0].main.temp)}°C
+            </span>
+            <span className="text-2xl font-medium">
+              {weather?.list[0].weather[0].description}
+            </span>
+            <span>Temp : {weather?.list[0]?.main.temp_max}°C / </span>
+            <span>Feels like : {weather?.list[0]?.main.feels_like}°C</span>
+            <span>Humidity : {weather?.list[0]?.main.humidity}%</span>
+            <span>Wind : {weather?.list[0]?.wind.speed}km/h</span>
           </div>
-        )}
+          <div className="flex flex-col p-5 m-5 text-white w-full bg-white bg-opacity-25 max-w-[300px] rounded-md">
+            <span className="text-4xl font-semibold text-white">
+              City : {weather?.city.name}
+            </span>
+            <span className="text-2xl mt-3 font-medium text-white">
+              Country : {weather?.city.country}
+            </span>
+            <input
+              onChange={handleChangeCity}
+              type="text"
+              placeholder="Enter your content"
+              className=" border border-slate-200 rounded-lg py-3 mt-3 px-5 outline-none  bg-transparent"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="weather-list grid grid-cols-4 absolute bottom-0 p-5 gap-10">
+        {weather?.list.map((item, index) => {
+          if (index % 8 === 0) {
+            return (
+              <WeatherItem
+                key={index}
+                day={new Date(item.dt_txt).toLocaleDateString("en-US", {
+                  weekday: "long",
+                })}
+                night={Math.round(item.main.temp_min)}
+                dayTemp={Math.round(item.main.temp_max)}
+                icon={item.weather[0].icon}
+              ></WeatherItem>
+            );
+          }
+        })}
       </div>
     </div>
   );
